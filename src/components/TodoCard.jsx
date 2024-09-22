3dff64b1d1165de57aadf3a15b3b1ff7394d316f
itSarthak <sarthakt3006@gmail.com>
2024-09-22 18:20:25 +0530

    Added Todo

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,20 @@
+export default function TodoCard(props) {
+  const { children, handleDeleteTodo, index } = props;
+  return (
+    <li className="todoItem">
+      {children}
+      <div className="actionsContainer">
+        <button>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button
+          onClick={() => {
+            handleDeleteTodo(index);
+          }}
+        >
+          <i className="fa-regular fa-trash-can"></i>
+        </button>
+      </div>
+    </li>
+  );
+}
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function TodoInput(props) {
+  const { handleAddTodos } = props;
+  const [todoValue, setTodoValue] = useState("");
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter todo..."
+      ></input>
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
+}
